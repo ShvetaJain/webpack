296eec0617c2e52236340b02017849122319f02f
ShvetaJain <jshveta@gmail.com>
2019-03-22 19:29:13 +0530

    Adds changes to - converts es5 functions to es6 arrow functions

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
-const es5 = function () {
-  function test1() {
-    const test = 'jain',
-      age = 80;
-    return `shveta ${test} ${age}`;
-  }
+const es5 = function() {
+    const test1 = () => {
+            const test = 'jain',
+                age = 80;
+            return `shveta ${test} ${age}`;
+        },
 
-  function objectTest() {
-    const url = 'test/test.html',
-      object = {
-        url,
-      };
-  }
+        objectTest = () => {
+            const url = 'test/test.html',
+                object = {
+                    url,
+                };
+        },
 
-  function destructure() {
-    const object = { name: 'Vishal', age: 24 },
-      { name, age } = object;
-  }
+        destructure = () => {
+            const object = {
+                    name: 'Vishal',
+                    age: 24
+                },
+                {
+                    name,
+                    age
+                } = object;
+        };
 };
